204028c79bfe8b3209f39b2fc7f32fadbaece047
Codematoin <joshjamison1@gmail.com>
2020-08-06 09:49:33 +0200

    removed pip search dependency by downloading latest wheel and inspecting version

--- nextbuild.py
+++ nextbuild.py
@@ -1,6 +1,13 @@
+"""
+Purpose:
+Increments current Pypi version by .001
+
+Usage: 
+    pip3 download aiopyql && ls aiopyql*.whl | sed 's/-/" "/g' | awk '{print "(" $2 ")"}' |  python3 python/aiopyql/aiopyql/nextbuild.py
+"""
 if __name__=='__main__':
     import sys
     version = sys.stdin.readline().rstrip()
     if '(' in version and ')' in version:
-        version = version[1:5]
-        print(f"{str(float(version)+0.001)[0:5]:.3f}")
+        version = version[2:7]
+        print(f"{float(version)+0.001:.3f}")
